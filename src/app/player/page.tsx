0220662c27f8e0f6aba9208eeca1e39442cc098a
ManuelCPinto <manuelcoelhopinto@gmail.com>
2024-11-14 09:36:57 +0000

    Revert "Merge pull request #4 from ManuelCPinto/feature/layout"
    This reverts commit 6bb802cfbe3b798ce8dba7180853c8648bd88d8d, reversing
    changes made to c3465c756f9df08827433f05a08eff7c243e5e8c.

--- src/app/player/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PLayer() {
-  return <p>Hello World!</p>
-}
--- src/components/Center.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react'
-import { clsx } from 'clsx'
-
-export default function Center({ children, className }: PropsWithChildren<{ className?: string }>) {
-  return <div className={clsx('flex flex-col items-center justify-center', className)}>{children}</div>
-}
