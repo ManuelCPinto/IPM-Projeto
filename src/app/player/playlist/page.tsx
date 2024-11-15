c413b317769cda38ee5122cc927d76624955d7e0
João Pedro Silveira <145754081+Joao-Pedro-Silveira@users.noreply.github.com>
2024-11-15 16:41:49 +0000

    Added playlist buttons

--- src/app/player/playlist/page.tsx
+++ src/app/player/playlist/page.tsx
@@ -43,6 +43,31 @@ function Playlist({ albumName, bandName, logoUrl, songs }: PlaylistProps) {
           <h2 className="band-name"><strong>{bandName}</strong></h2>
         </div>
       </div>
+      <div className='playlist-options'>
+        <button className="playlist-play-button">
+          <div className="playlist-play-triangle-icon"></div>
+        </button>
+        <button className="shuffle-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20" height="20" /* Adjust size as needed */
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polyline points="16 3 21 3 21 8"></polyline>
+            <line x1="4" y1="20" x2="21" y2="3"></line>
+            <polyline points="21 16 21 21 16 21"></polyline>
+            <line x1="15" y1="15" x2="21" y2="21"></line>
+            <line x1="4" y1="4" x2="9" y2="9"></line>
+          </svg>
+        </button>
+        <button className="playlist-like-button">♥</button>
+        <button className="playlist-options-button">⋮</button>
+      </div>
       <div className="table-header-container">
         <div className="table-header-item">
           <div className="table-header-row">
@@ -65,6 +90,7 @@ function Playlist({ albumName, bandName, logoUrl, songs }: PlaylistProps) {
               <div className="song-album">{song.album}</div>
               <div className="song-duration">{song.duration}</div>
             </div>
+            <button className="like-button">♥</button>
             <button className="options-button">⋮</button>
           </div>
         ))}
