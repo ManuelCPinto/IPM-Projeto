394916e574f040650626f7f4ee790f7c853d377d
João Pedro Silveira <145754081+Joao-Pedro-Silveira@users.noreply.github.com>
2024-11-19 12:59:58 +0000

    formating

--- src/app/player/layout.tsx
+++ src/app/player/layout.tsx
@@ -10,25 +10,21 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <html lang="en">
-    <body>
-      <div className="grid h-screen grid-rows-[max-content_1fr_max-content] grid-cols-[max-content_1fr] gap-0">
-        {/* Navbar */}
-        <div className="col-span-2">
-          <Navbar />
-        </div>
-        <Sidebar />
-        {/* Content Area */}
-        <main className="flex p-8 overflow-y-auto bg-neutral-900 bg-[radial-gradient(ellipse_75%_75%_at_50%_50%,rgba(30,30,50,0.6),rgba(15,15,30,1),rgba(5,5,15,1))] shadow-lg">
-          { children }
-        </main>
-        {/* Music Player */}
-        <footer className="col-span-2">
-          <MusicPlayerBar />
-        </footer>
-      </div>
-    </body>
-  </html>
+  <div className="grid h-screen grid-rows-[max-content_1fr_max-content] grid-cols-[max-content_1fr] gap-0">
+    {/* Navbar */}
+    <div className="col-span-2">
+      <Navbar />
+    </div>
+    <Sidebar />
+    {/* Content Area */}
+    <main className="flex p-8 overflow-y-auto bg-neutral-900 bg-[radial-gradient(ellipse_75%_75%_at_50%_50%,rgba(30,30,50,0.6),rgba(15,15,30,1),rgba(5,5,15,1))] shadow-lg">
+      {children}
+    </main>
+    {/* Music Player */}
+    <footer className="col-span-2 overflow-hidden">
+      <MusicPlayerBar />
+    </footer>
+  </div>
 )
 
 export default Layout
--- src/app/player/playlist/[playlistId]/page.tsx
+++ src/app/player/playlist/[playlistId]/page.tsx
@@ -35,38 +35,40 @@ function Playlist({ albumName, bandName, logoUrl, songs }: PlaylistProps) {
   if (!logoUrl) return <div>Missing logo URL</div>;
 
   return (
-    <div>
+    <div className='main'>
       <div className="album-header">
-        <img src={logoUrl} className="playlist-logo" alt="Playlist Logo" />
-        <div className="band-info">
-          <h1 className="album-name"><strong>{albumName}</strong></h1>
-          <h2 className="band-name"><strong>{bandName}</strong></h2>
+        <div className='album-info'>
+          <img src={logoUrl} className="playlist-logo" alt="Playlist Logo" />
+          <div className="band-info">
+            <h1 className="album-name">{albumName}</h1>
+            <h2 className="band-name"><strong>{bandName}</strong></h2>
+          </div>
         </div>
-      </div>
-      <div className='playlist-options'>
-        <button className="playlist-play-button">
-          <div className="playlist-play-triangle-icon"></div>
-        </button>
-        <button className="shuffle-button">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20" height="20" /* Adjust size as needed */
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <polyline points="16 3 21 3 21 8"></polyline>
-            <line x1="4" y1="20" x2="21" y2="3"></line>
-            <polyline points="21 16 21 21 16 21"></polyline>
-            <line x1="15" y1="15" x2="21" y2="21"></line>
-            <line x1="4" y1="4" x2="9" y2="9"></line>
-          </svg>
-        </button>
-        <button className="playlist-like-button">♥</button>
-        <button className="playlist-options-button">⋮</button>
+        <div className='playlist-options'>
+          <button className="playlist-play-button">
+            <div className="playlist-play-triangle-icon"></div>
+          </button>
+          <button className="shuffle-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20" height="20" /* Adjust size as needed */
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <polyline points="16 3 21 3 21 8"></polyline>
+              <line x1="4" y1="20" x2="21" y2="3"></line>
+              <polyline points="21 16 21 21 16 21"></polyline>
+              <line x1="15" y1="15" x2="21" y2="21"></line>
+              <line x1="4" y1="4" x2="9" y2="9"></line>
+            </svg>
+          </button>
+          <button className="playlist-like-button">♥</button>
+          <button className="playlist-options-button">⋮</button>
+        </div>
       </div>
       <div className="table-header-container">
         <div className="table-header-item">
@@ -87,7 +89,10 @@ function Playlist({ albumName, bandName, logoUrl, songs }: PlaylistProps) {
             <div className="song-info">
               <div className="song-title">{song.title}</div>
               <div className="song-artist">{song.artist}</div>
-              <div className="song-album">{song.album}</div>
+              <div>
+                <div className="song-album">{song.album}</div>
+                
+              </div>
               <div className="song-duration">{song.duration}</div>
             </div>
             <button className="like-button">♥</button>
