d9677ed53fddf8f24ba0e713bdd887d7e36bb67b
João Pedro Silveira <145754081+Joao-Pedro-Silveira@users.noreply.github.com>
2024-11-20 16:08:19 +0000

    altered shuffle icon

--- src/app/player/playlist/[playlistId]/page.tsx
+++ src/app/player/playlist/[playlistId]/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import './playlist.css';
 import './playlistHeader.css';
+import { FaShuffle } from "react-icons/fa6";
+
 
 interface PlaylistProps {
   name: string;
@@ -84,22 +86,7 @@ function Playlist({ name, author, imageURL, songs }: PlaylistProps) {
             <div className="playlist-play-triangle-icon"></div>
           </button>
           <button className="shuffle-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20" height="20" /* Adjust size as needed */
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <polyline points="16 3 21 3 21 8"></polyline>
-              <line x1="4" y1="20" x2="21" y2="3"></line>
-              <polyline points="21 16 21 21 16 21"></polyline>
-              <line x1="15" y1="15" x2="21" y2="21"></line>
-              <line x1="4" y1="4" x2="9" y2="9"></line>
-            </svg>
+            <FaShuffle />
           </button>
           <button className="playlist-like-button">♥</button>
           <button className="playlist-options-button">⋮</button>
