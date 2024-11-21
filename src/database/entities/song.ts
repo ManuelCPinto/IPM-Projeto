bf10fe33f4bfd2a140924d5e4db935123f6009ed
MrG0ld <42754748+MrG0ld@users.noreply.github.com>
2024-11-21 14:20:53 +0000

    Fixed and changed database

--- src/database/entities/song.ts
+++ src/database/entities/song.ts
@@ -1,7 +1,8 @@
 // song.ts
 
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-import { albumsTable } from './album';
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
+import { albumsTable } from './album'
+import { usersTable } from '@/database/entities/user'
 
 export const songsTable = sqliteTable('songs', {
   id: integer('id').primaryKey({ autoIncrement: true }),
@@ -12,6 +13,11 @@ export const songsTable = sqliteTable('songs', {
   name: text('name').notNull(),
   duration: text('duration').notNull(),
   feature: text('feature'),
-});
+  author: text('author')
+    .notNull()
+    .references(() => usersTable.username, { onDelete: 'cascade' }),
+  cover: text(),
+  audio: text()
+})
 
-export type Song = typeof songsTable.$inferSelect;
+export type Song = typeof songsTable.$inferSelect
